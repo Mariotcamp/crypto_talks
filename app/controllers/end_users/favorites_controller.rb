bd5983fb900c02074eea21c7ba5d6e236cfc449a
EC2 Default User <ec2-user@ip-172-31-37-179.ap-northeast-1.compute.internal>
2021-09-03 06:45:15 +0000

    いいねボタンajax実装完了

--- app/controllers/end_users/favorites_controller.rb
+++ app/controllers/end_users/favorites_controller.rb
@@ -8,18 +8,16 @@ class EndUsers::FavoritesController < ApplicationController
   end
 
   def create
-    post = Post.find(params[:post_id])
-    favorite = current_end_user.favorites.new(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = current_end_user.favorites.new(post_id: @post.id)
     favorite.save
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    if post.have_favorite?(current_end_user)
-      favorite = current_end_user.favorites.find_by(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    if @post.have_favorite?(current_end_user)
+      favorite = current_end_user.favorites.find_by(post_id: @post.id)
       favorite.destroy
-      redirect_back(fallback_location: root_path)
     else
       redirect_back(fallback_location: root_path)
     end
