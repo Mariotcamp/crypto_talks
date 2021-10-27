a65bebae36021cf5119e536f2586ed8734136a55
EC2 Default User <ec2-user@ip-172-31-37-179.ap-northeast-1.compute.internal>
2021-10-27 10:31:01 +0000

    CSRF改善1

--- app/controllers/admin_users/home_controller.rb
+++ app/controllers/admin_users/home_controller.rb
@@ -1,6 +1,6 @@
 class AdminUsers::HomeController < ApplicationController
   before_action :logged_in_end_user, :finished_log_in_as_admin, :confirm_number_of_quizes
-
+  protect_from_forgery :except => [:top]
   def top
   end
 
